e2824b4d4e8553b3ecac5762705a6cea445c1c82
JessicaS161995 <ojitos16@outlook.es>
2023-11-09 15:09:32 -0500

    update

--- clases/Particula.js
+++ clases/Particula.js
@@ -7,6 +7,26 @@ class Particula{
     }
     update() {
         this.r = random();
+
+        if (this.r < 0.25) {
+            //se mueve hacia arriba
+            this.pos.y -= this.side;
+        } else if (this.r < 0.5) {
+            //hacia la derecha
+            this.pos.x += this.side;
+        } else if (this.r < 0.75) {
+            //hacia abajo
+            this.pos.y += this.side;    
+        } else {
+            //hacia la izquierda
+            this.pos.x -= this.side;   
+        }
+        
+        this.lifespan -= 1;
+
+        if (this.lifespan <= 0) {
+            this.isAlive = false;
+        }
     }
     display() {
         square(this.pos.x, this.pos.y, this.side);
